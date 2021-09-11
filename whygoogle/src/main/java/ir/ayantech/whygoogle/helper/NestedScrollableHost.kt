8847a341257af28b93ab343a46e53e02f431e878
A.Badakhshan <ugdvqh123>
2021-09-11 13:49:21 +0430

    swipable for test only 0.2.5

--- whygoogle/src/main/java/ir/ayantech/whygoogle/helper/NestedScrollableHost.kt
+++ whygoogle/src/main/java/ir/ayantech/whygoogle/helper/NestedScrollableHost.kt
@@ -2,16 +2,31 @@ package ir.ayantech.whygoogle.helper
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
+import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.ORIENTATION_HORIZONTAL
+import kotlin.math.absoluteValue
+import kotlin.math.sign
 
+/**
+ * Layout to wrap a scrollable component inside a ViewPager2. Provided as a solution to the problem
+ * where pages of ViewPager2 have nested scrollable elements that scroll in the same direction as
+ * ViewPager2. The scrollable element needs to be the immediate and only child of this host layout.
+ *
+ * This solution has limitations when using multiple levels of nested scrollable elements
+ * (e.g. a horizontal RecyclerView in a vertical RecyclerView in a horizontal ViewPager2).
+ */
 class NestedScrollableHost : FrameLayout {
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
 
+    private var touchSlop = 0
+    private var initialX = 0f
+    private var initialY = 0f
     private val parentViewPager: ViewPager2?
         get() {
             var v: View? = parent as? View
@@ -21,24 +36,89 @@ class NestedScrollableHost : FrameLayout {
             return v as? ViewPager2
         }
 
+    private fun getFirstScrollableChild(viewToCheck: ViewGroup = this): View? {
+        val orientation = parentViewPager?.orientation ?: return null
+        for (i in (viewToCheck.childCount - 1)..0) {
+            val childToCheck = viewToCheck.getChildAt(i)
+            if (childToCheck is ViewGroup) {
+                val grandChild = getFirstScrollableChild(childToCheck)
+                if (grandChild != null) return grandChild
+            }
+            if (canViewScroll(childToCheck, orientation, -1f) && canViewScroll(
+                    childToCheck,
+                    orientation,
+                    1f
+                )
+            )
+                return childToCheck
+        }
+        return null
+    }
+
+    init {
+        touchSlop = ViewConfiguration.get(context).scaledTouchSlop
+    }
+
+    private fun canViewScroll(view: View?, orientation: Int, delta: Float): Boolean {
+        val direction = -delta.sign.toInt()
+        return when (orientation) {
+            0 -> view?.canScrollHorizontally(direction) ?: false
+            1 -> view?.canScrollVertically(direction) ?: false
+            else -> throw IllegalArgumentException()
+        }
+    }
+
     override fun onInterceptTouchEvent(e: MotionEvent): Boolean {
         handleInterceptTouchEvent(e)
         return super.onInterceptTouchEvent(e)
     }
 
     private fun handleInterceptTouchEvent(e: MotionEvent) {
-        when (e.action) {
-            MotionEvent.ACTION_DOWN -> {
-                parentViewPager?.isUserInputEnabled = false
-                Log.d("uie", "false")
-            }
-            MotionEvent.ACTION_UP -> {
-                parentViewPager?.isUserInputEnabled = true
-                Log.d("uie", "true")
-            }
-            MotionEvent.ACTION_CANCEL -> {
-                parentViewPager?.isUserInputEnabled = true
-                Log.d("uie", "true")
+        val orientation = parentViewPager?.orientation ?: return
+
+        val firstScrollableChild = getFirstScrollableChild()
+        // Early return if child can't scroll in same direction as parent
+        if (!canViewScroll(firstScrollableChild, orientation, -1f) && !canViewScroll(
+                firstScrollableChild,
+                orientation,
+                1f
+            )
+        ) {
+            return
+        }
+
+        if (e.action == MotionEvent.ACTION_DOWN) {
+            initialX = e.x
+            initialY = e.y
+            parent.requestDisallowInterceptTouchEvent(true)
+        } else if (e.action == MotionEvent.ACTION_MOVE) {
+            val dx = e.x - initialX
+            val dy = e.y - initialY
+            val isVpHorizontal = orientation == ORIENTATION_HORIZONTAL
+
+            // assuming ViewPager2 touch-slop is 2x touch-slop of child
+            val scaledDx = dx.absoluteValue * if (isVpHorizontal) .5f else 1f
+            val scaledDy = dy.absoluteValue * if (isVpHorizontal) 1f else .5f
+
+            if (scaledDx > touchSlop || scaledDy > touchSlop) {
+                if (isVpHorizontal == (scaledDy > scaledDx)) {
+                    // Gesture is perpendicular, allow all parents to intercept
+                    parent.requestDisallowInterceptTouchEvent(false)
+                } else {
+                    // Gesture is parallel, query child if movement in that direction is possible
+                    if (canViewScroll(
+                            firstScrollableChild,
+                            orientation,
+                            if (isVpHorizontal) dx else dy
+                        )
+                    ) {
+                        // Child can scroll, disallow all parents to intercept
+                        parent.requestDisallowInterceptTouchEvent(true)
+                    } else {
+                        // Child cannot scroll, allow all parents to intercept
+                        parent.requestDisallowInterceptTouchEvent(false)
+                    }
+                }
             }
         }
     }
